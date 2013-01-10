94c8a245a578070d3e66c4ec5fd00954572d5584
Cheng Huang <cheng_huang@college.harvard.edu>
2013-01-10 17:38:34 -0500

    merged draftone and encampment

--- teams/encampment/RobotPlayer.java
+++ teams/encampment/RobotPlayer.java
@@ -5,8 +5,8 @@ import java.util.Random;
 import battlecode.common.*;
 
 public class RobotPlayer {
-	private static final int MAX_SOLDIERS = 200;
-	private static final int RALLY_TIME = 200;
+	private static final int MAX_SOLDIERS = 10;
+	private static final int RALLY_TIME = 30;
 	private static RobotController rc;
 	private static MapLocation offenseRallyPoint = null; 
 	private static MapLocation defenseRallyPoint = null; 
@@ -16,8 +16,6 @@ public class RobotPlayer {
 	private static int totalSoldiers = 0; 
 	private static int halfDistBetweenHQ = -1;  //TODO: check if this is actually half
 	private static MapLocation[] neutralEncampmentSquares = null;
-	private static Random randGen = new Random();
-	private static int encampmentTypeCounter;
 
 			
 	public static void run(RobotController myRC) {
@@ -28,56 +26,50 @@ public class RobotPlayer {
 		
 		// only set these values once at the beginning of the game
 		if (enemyHQ == null) { 
-			enemyHQ = rc.senseEnemyHQLocation();
-			homeHQ = rc.senseHQLocation();
-			halfDistBetweenHQ = DistBetweenHQ() / 4;
-			encampmentTypeCounter = 0;
-			try {
-				neutralEncampmentSquares = rc.senseEncampmentSquares(homeHQ, 50, Team.NEUTRAL);
-			} catch (GameActionException e1) {
-				// e1.printStackTrace();
-			}	
+//			enemyHQ = rc.senseEnemyHQLocation();
+//			homeHQ = rc.senseHQLocation();
+//			halfDistBetweenHQ = DistBetweenHQ() / 4;
+//			try {
+//				neutralEncampmentSquares = rc.senseEncampmentSquares(homeHQ, 100, Team.NEUTRAL);
+//			} catch (GameActionException e1) {
+//				// e1.printStackTrace();
+//			}	
 		}
 		
 		while(true) {
 			try{
 				if (rc.getType() == RobotType.SOLDIER) {
 					int curID = rc.getRobot().getID();
-					
-					if (Clock.getRoundNum() < RALLY_TIME) { 
-						// capture encampment if on it
-						
-						if (rc.senseEncampmentSquare(rc.getLocation())) {
-							rc.captureEncampment(chooseEncampmentType());
-						}
-
-						// go to nearbyEncampmentSquare if nearby
-						int neutralEncampmentLen = neutralEncampmentSquares.length;
-						
-						if (neutralEncampmentLen > 0) {
-							MapLocation anEncampment = neutralEncampmentSquares[curID % neutralEncampmentLen];
-							goToLocation(anEncampment);	
-						}
-						
-						
-						// else go to a rally point
-						else {
-							// odd is offense, even is defense
-							if (curID % 2 == 1) 
-								goToLocation(offenseRallyPoint);
-							else
-								goToLocation(defenseRallyPoint);
-						}
-					}
-					
-					else {
-						if (curID % 2 == 1) 
+//					if (Clock.getRoundNum() < RALLY_TIME) { 
+//						// capture encampment if on it
+//						if (rc.senseEncampmentSquare(rc.getLocation())) {
+//							rc.captureEncampment(chooseEncampmentType());
+//							rc.yield();
+//						}
+//						// go to nearbyEncampmentSquare if nearby
+//						int neutralEncampmentLen = neutralEncampmentSquares.length;
+//						if (neutralEncampmentLen > 0) {
+//							MapLocation anEncampment = neutralEncampmentSquares[curID % neutralEncampmentLen];
+//							goToLocation(anEncampment);	
+//						}
+//						// else go to a rally point
+//						else {
+//							// odd is offense, even is defense
+//							if (curID % 2 == 1) 
+//								goToLocation(offenseRallyPoint);
+//							else
+//								goToLocation(defenseRallyPoint);
+//						}
+//					}
+//					
+//					else {
+						if (curID % 3 != 0) 
 							offense(curID / 2);
 						else 
 							defense(curID / 2);
-					}
+//					}
 				} 
-				else {
+				else if (rc.getType() == RobotType.HQ){
 					Robot[] enemiesNearHome = rc.senseNearbyGameObjects(Robot.class,1000000,rc.getTeam().opponent());
 					closestEnemyNearHome = findClosest(homeHQ, enemiesNearHome);
 					hqCode();
@@ -89,6 +81,7 @@ public class RobotPlayer {
 				e.printStackTrace();
 			}
 			// necessary? 
+			rc.yield();
 		}					
 	}
 	
@@ -97,6 +90,7 @@ public class RobotPlayer {
 	 */
 	// do we need to pass Id to this?
 	private static void defense(int idNum) throws GameActionException {
+
 		if (closestEnemyNearHome != null && 
 			closestEnemyNearHome.distanceSquaredTo(homeHQ) < halfDistBetweenHQ) {
 			// attack the closest enemy if enemy cross the line
@@ -110,7 +104,7 @@ public class RobotPlayer {
 
 	private static RobotType chooseEncampmentType() {
 		RobotType[] encampmentTypes = {RobotType.SUPPLIER, RobotType.GENERATOR};
-		if (Math.random() < 0.9) {
+		if (Clock.getRoundNum() % 2 == 1) {
 			// generator
 			return encampmentTypes[1];
 		}
@@ -120,9 +114,21 @@ public class RobotPlayer {
 	}
 	
 	private static void offense(int idNum) throws GameActionException { 
-		// pass divided by 2
-		// stay in enemy half
-		if (idNum % 2 == 1) {
+
+		// try to defuse mine
+		Direction dirToEnemyHQ = rc.getLocation().directionTo(enemyHQ);
+		Team mineAtLocation = rc.senseMine(rc.getLocation().add(dirToEnemyHQ));
+		if (mineAtLocation != null) {
+			rc.defuseMine(rc.getLocation());
+		}
+		// try to capture encampment
+		else if (rc.senseEncampmentSquare(rc.getLocation())) {
+			rc.captureEncampment(chooseEncampmentType());
+			return; 
+		}
+
+		// move toward enemy hq 		
+		else if (idNum % 2 == 1) {
 			// attack HQ
 			goToLocation(enemyHQ); //TODO: optimize			
 		} else {
@@ -209,8 +215,5 @@ public class RobotPlayer {
 		MapLocation rallyPoint = new MapLocation(x,y);
 		return rallyPoint;
 	}
-
-
-
 }
 
