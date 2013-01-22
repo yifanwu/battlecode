60f3934a3320c784d03213fca7ba0a15757d0884
chuang90 <chuang90@gmail.com>
2013-01-21 19:54:06 -0500

    Readded random spawning

--- teams/team105/HQBot.java
+++ teams/team105/HQBot.java
@@ -10,11 +10,24 @@ public class HQBot {
 		rc = myRC;
 		enemyHQ = rc.senseEnemyHQLocation();
 		if (rc.isActive() &&rc.getTeamPower() > 50) {
-			Direction dir = rc.getLocation().directionTo(enemyHQ);
+			Direction dir = getDirForSpawn(enemyHQ);
 			if (rc.canMove(dir)) {
 				rc.spawn(dir);
 			}
 		}
 		rc.yield();
 	}
+	
+	private static Direction getDirForSpawn(MapLocation enemyHQ) {
+		// randomize the direction
+		double rand = Math.random();
+		Direction dir = rc.getLocation().directionTo(enemyHQ);
+		if (rand < 0.25) {
+			dir = dir.rotateRight();
+		} else if (rand < 0.5) {
+			dir = dir.rotateLeft();							
+		}
+		return dir;
+	}
+	
 }
